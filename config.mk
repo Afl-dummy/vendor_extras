3c539aaa7ee7b779271eaa6f9f4d1a144105865a
HDzungx <hdzungx@gmail.com>
2025-12-05 16:11:00 +0700

    Themes: Import Fonts
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: KuroeHanako <78864678+kuroehanako@users.noreply.github.com>
    Co-authored-by: Lup Gabriel <gwolf2u.website@gmail.com>
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Co-authored-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Co-authored-by: Raphiel Rollerscaperers <rapherion@raphielgang.org>
    Co-authored-by: Subratam6296 <padraramesh420@gmail.com>
    Co-authored-by: spkal01 <kalligeross@gmail.com>
    Co-authored-by: DrDisagree <crazymahmud08@gmail.com>
    Co-authored-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: HinohArata <mmgcntk@gmail.com>

--- config.mk
+++ config.mk
@@ -18,3 +18,63 @@ PRODUCT_PACKAGES += \
     SystemUIClocks-Metro \
     SystemUIClocks-NumOverlap
 #    SystemUIClocks-Weather
+
+# Fonts
+PRODUCT_PACKAGES += \
+    fonts_customization.xml \
+    FontAccuratistOverlay \
+    FontAclonicaOverlay \
+    FontAmaranteOverlay \
+    FontBariolOverlay \
+    FontCagliostroOverlay \
+    FontCoconOverlay \
+    FontComfortaaOverlay \
+    FontComicSansOverlay \
+    FontCookieRunOverlay \
+    FontCooljazzOverlay \
+    FontCoolstoryOverlay \
+    FontExotwoOverlay \
+    FontEvoSansOverlay \
+    FontEvolveSansOverlay \
+    FontFifa2018Overlay \
+    FontGrandHotelOverlay \
+    FontGeneralSansOverlay \
+    FontGoogleSansFlexOverlay \
+    FontHarmonySansOverlay \
+    FontIBMPlexSansOverlay \
+    FontLatoOverlay \
+    FontLinotteOverlay \
+    FontNokiaPureOverlay \
+    FontNothingDotHeadlineOverlay \
+    FontNothingDotOverlay \
+    FontNunitoOverlay \
+    FontOneUISansOverlay \
+    FontOneplusSansOverlay \
+    FontOneplusSlateOverlay \
+    FontPoppinsSourceOverlay \
+    FontOswaldOverlay \
+    FontPlayOverlay \
+    FontQuandoOverlay \
+    FontRedressedOverlay \
+    FontReemKufiOverlay \
+    FontRobotoCondensedOverlay \
+    FontRookeryOverlay \
+    FontRubikOverlay \
+    FontSamsungOneOverlay \
+    FontSanFranciscoDisplayProSourceOverlay \
+    FontSansSerifOverlay \
+    FontSansSerifProOverlay \
+    FontSonySketchOverlay \
+    FontSpaceGroteskOverlay \
+    FontStoropiaOverlay \
+    FontSurferOverlay \
+    FontUbuntuOverlay \
+    FontVolteOverlay
+
+# Include {GoogleSansFlex,Lato,Rubik} fonts
+$(call inherit-product-if-exists, external/google-fonts/google-sans-flex/fonts.mk)
+$(call inherit-product-if-exists, external/google-fonts/lato/fonts.mk)
+$(call inherit-product-if-exists, external/google-fonts/rubik/fonts.mk)
+
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,vendor/extras/prebuilt/product/fonts,$(TARGET_COPY_OUT_PRODUCT)/fonts)
