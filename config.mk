44b15bbf5300f879bf8000687ee9ec7e9459c607
HDzungx <hdzungx@gmail.com>
2025-12-07 20:29:02 +0700

    themes: support black theme for dark mode [3/3]
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: Abhay Singh Gill <abhaygill017@gmail.com>
    Signed-off-by: HinohArata <mmgcntk@gmail.com>

--- config.mk
+++ config.mk
@@ -79,3 +79,7 @@ $(call inherit-product-if-exists, external/google-fonts/rubik/fonts.mk)
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,vendor/extras/prebuilt/product/fonts,$(TARGET_COPY_OUT_PRODUCT)/fonts) \
     $(call find-copy-subdir-files,*,vendor/extras/prebuilt/product/media/audio/ui,$(TARGET_COPY_OUT_PRODUCT)/media/audio/ui)
+
+# Themes
+PRODUCT_PACKAGES += \
+    AndroidBlackThemeOverlay
