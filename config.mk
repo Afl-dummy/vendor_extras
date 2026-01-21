fb0b3a49636f265698d5817fc97a28fa07d55805
HinohArata <mmgcntk@gmail.com>
2026-01-21 13:38:28 +0700

    extras: Build Moto Calculator and Via Browser in vanilla
    Signed-off-by: HinohArata <mmgcntk@gmail.com>

--- config.mk
+++ config.mk
@@ -1,2 +1,9 @@
 LOCAL_PATH := $(call my-dir)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
+
+# MC and VB
+ifneq ($(WITH_GMS),true)
+PRODUCT_PACKAGES += \
+    MotoCalculator \
+    ViaBrowser
+endif
